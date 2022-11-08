b615743e3686d980fca8e5f8a674cf4850d827a6
Yuuma <y.sato.sys22@morijyobi.ac.jp>
2022-11-08 12:15:12 +0900

    中身の追加

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main{
 	public static void main(String[] args){
 		double bmi = 47 / (1.6 * 1.6);
+		double bmi2 = 52 / (1.7 * 1.7);
 		System.out.println((double)Math.round(bmi*100)/100);
+		System.out.println((double)Math.round(bmi2*100)/100);
 	}
 }
